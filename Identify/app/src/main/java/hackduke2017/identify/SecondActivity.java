63a7013e608563400e7ae196d97ed49b9b320591
Kyle Newman <kyle.newman@duke.edu>
2017-10-28 17:26:53 -0400

    Image browse working

--- Identify/app/src/main/java/hackduke2017/identify/SecondActivity.java
+++ Identify/app/src/main/java/hackduke2017/identify/SecondActivity.java
@@ -1,151 +1,153 @@
+
 package hackduke2017.identify;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-        import android.content.Intent;
-        import android.database.Cursor;
-        import android.graphics.Bitmap;
-        import android.graphics.BitmapFactory;
-        import android.net.Uri;
-        import android.os.ParcelFileDescriptor;
-        import android.os.Parcelable;
-        import android.provider.MediaStore;
-        import android.support.v7.app.ActionBarActivity;
-        import android.os.Bundle;
-        import android.util.Log;
-        import android.view.View;
-        import android.widget.ImageView;
-        import android.widget.Toast;
+public class SecondActivity extends Activity {
 
-        import java.io.FileDescriptor;
-        import java.io.IOException;
-        import java.util.ArrayList;
+    private static int IMG_RESULT = 1;
+    String ImageDecode;
+    ImageView imageViewLoad;
+    Button LoadImage;
+    Intent intent;
+    public String[] FILE;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
 
-/**
- * Example code for picking images in android
- */
-public class SecondActivity extends ActionBarActivity {
+        imageViewLoad = (ImageView) findViewById(R.id.imageView1);
+        LoadImage = (Button)findViewById(R.id.button1);
 
-    // this is the action code we use in our intent,
-    // this way we know we're looking at the response from our own action
-    private static final int SELECT_SINGLE_PICTURE = 101;
+        LoadImage.setOnClickListener(new View.OnClickListener() {
 
-    private static final int SELECT_MULTIPLE_PICTURE = 201;
+            @Override
+            public void onClick(View v) {
 
-    public static final String IMAGE_TYPE = "image/*";
+                intent = new Intent(Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
 
-    private ImageView selectedImagePreview;
+                startActivityForResult(intent, IMG_RESULT);
+
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        try {
+
+            if (requestCode == IMG_RESULT && resultCode == RESULT_OK
+                    && null != data) {
+
+
+                Uri URI = data.getData();
+                String[] FILE = { MediaStore.Images.Media.DATA };
+
+
+                Cursor cursor = getContentResolver().query(URI,
+                        FILE, null, null, null);
+
+                cursor.moveToFirst();
+
+                int columnIndex = cursor.getColumnIndex(FILE[0]);
+                ImageDecode = cursor.getString(columnIndex);
+                cursor.close();
+
+                imageViewLoad.setImageBitmap(BitmapFactory
+                        .decodeFile(ImageDecode));
+
+                Toast.makeText(this, "Image Selected", Toast.LENGTH_LONG)
+                        .show();
+
+            }
+        } catch (Exception e) {
+            Toast.makeText(this, "Please try again", Toast.LENGTH_LONG)
+                    .show();
+        }
+
+    }
+
+    public String[] getFILE()
+    {
+        return FILE;
+    }
+
+}
+
+
+/*
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class SecondActivity extends Activity {
+
+
+    private static int RESULT_LOAD_IMAGE = 1;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_second);
 
-        // no need to cast to button view here since we can add a listener to any view, this
-        // is the single image selection
-        findViewById(R.id.button1).setOnClickListener(new View.OnClickListener() {
+        Button buttonLoadImage = (Button) findViewById(R.id.buttonLoadPicture);
+        buttonLoadImage.setOnClickListener(new View.OnClickListener() {
 
+            @Override
             public void onClick(View arg0) {
 
-                // in onCreate or any event where your want the user to
-                // select a file
-                Intent intent = new Intent();
-                intent.setType(IMAGE_TYPE);
-                intent.setAction(Intent.ACTION_GET_CONTENT);
-                startActivityForResult(Intent.createChooser(intent,
-                        getString(R.string.select_picture)), SELECT_SINGLE_PICTURE);
+                Intent i = new Intent(
+                        Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+                startActivityForResult(i, RESULT_LOAD_IMAGE);
             }
         });
-
-        // multiple image selection
-        findViewById(R.id.button2).setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-
-                Intent intent = new Intent();
-                intent.setType(IMAGE_TYPE);
-                intent.setAction(Intent.ACTION_GET_CONTENT);
-                // this line is different here !!
-                intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
-                startActivityForResult(Intent.createChooser(intent,
-                        getString(R.string.select_picture)), SELECT_MULTIPLE_PICTURE);
-            }
-        });
-
-        selectedImagePreview = (ImageView)findViewById(R.id.image_preview);
     }
 
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (resultCode == RESULT_OK) {
-            if (requestCode == SELECT_SINGLE_PICTURE) {
 
-                Uri selectedImageUri = data.getData();
-                try {
-                    selectedImagePreview.setImageBitmap(new UserPicture(selectedImageUri, getContentResolver()).getBitmap());
-                } catch (IOException e) {
-                    Log.e(MainActivity.class.getSimpleName(), "Failed to load image", e);
-                }
-                // original code
-//                String selectedImagePath = getPath(selectedImageUri);
-//                selectedImagePreview.setImageURI(selectedImageUri);
-            }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
-        } else {
-            // report failure
-            Toast.makeText(getApplicationContext(), R.string.msg_failed_to_get_intent_data, Toast.LENGTH_LONG).show();
-            Log.d(MainActivity.class.getSimpleName(), "Failed to get intent data, result code is " + resultCode);
-        }
-    }
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = { MediaStore.Images.Media.DATA };
 
-    /**
-     * helper to retrieve the path of an image URI
-     */
-    public String getPath(Uri uri) {
-
-        // just some safety built in
-        if( uri == null ) {
-            // perform some logging or show user feedback
-            Toast.makeText(getApplicationContext(), R.string.msg_failed_to_get_picture, Toast.LENGTH_LONG).show();
-            Log.d(MainActivity.class.getSimpleName(), "Failed to parse image path from image URI " + uri);
-            return null;
-        }
-
-        // try to retrieve the image from the media store first
-        // this will only work for images selected from gallery
-        String[] projection = { MediaStore.Images.Media.DATA };
-        Cursor cursor = managedQuery(uri, projection, null, null, null);
-        if( cursor != null ){
-            int column_index = cursor
-                    .getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            Cursor cursor = getContentResolver().query(selectedImage,
+                    filePathColumn, null, null, null);
             cursor.moveToFirst();
-            return cursor.getString(column_index);
+
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String picturePath = cursor.getString(columnIndex);
+            cursor.close();
+
+            ImageView imageView = (ImageView) findViewById(R.id.imgView);
+            imageView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+
         }
-        // this is our fallback here, thanks to the answer from @mad indicating this is needed for
-        // working code based on images selected using other file managers
-        return uri.getPath();
+
+
     }
-
-
-//    /**
-//     * helper to scale down image before display to prevent render errors:
-//     * "Bitmap too large to be uploaded into a texture"
-//     */
-//    private void displayPicture(String imagePath, ImageView imageView) {
-//
-//        // from http://stackoverflow.com/questions/22633638/prevent-bitmap-too-large-to-be-uploaded-into-a-texture-android
-//
-//        BitmapFactory.Options options = new BitmapFactory.Options();
-//        options.inSampleSize = 4;
-//
-//        Bitmap bitmap = BitmapFactory.decodeFile(imagePath);
-//        int height = bitmap.getHeight(), width = bitmap.getWidth();
-//
-//        if (height > 1280 && width > 960){
-//            Bitmap imgbitmap = BitmapFactory.decodeFile(imagePath, options);
-//            imageView.setImageBitmap(imgbitmap);
-//        } else {
-//            imageView.setImageBitmap(bitmap);
-//        }
-//    }
-
-
-}
+}*/
