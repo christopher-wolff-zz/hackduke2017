ac7862b5b5bc4d4ce55bc479615a60c9cda47578
Kyle Newman <kyle.newman@duke.edu>
2017-10-28 18:14:36 -0400

    merged app opening and photo browser

--- Identify/app/src/main/java/hackduke2017/identify/SecondActivity.java
+++ Identify/app/src/main/java/hackduke2017/identify/SecondActivity.java
@@ -1,53 +1,87 @@
 package hackduke2017.identify;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
-import java.io.*;
-import android.app.*;
-import android.content.*;
-import android.net.*;
-import android.os.*;
-import android.view.*;
-import android.graphics.*;
-import android.widget.*;
-import android.provider.*;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class SecondActivity extends AppCompatActivity {
+public class SecondActivity extends Activity {
 
-    private final int PICK_IMAGE = 1;
-    private ProgressDialog detectionProgressDialog;
-
+    private static int IMG_RESULT = 1;
+    String ImageDecode;
+    ImageView imageViewLoad;
+    Button LoadImage;
+    Intent intent;
+    public String[] FILE;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Button button1 = (Button)findViewById(R.id.button1);
-        button1.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_second);
+
+        imageViewLoad = (ImageView) findViewById(R.id.imageView1);
+        LoadImage = (Button)findViewById(R.id.button1);
+
+        LoadImage.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent();
-                // Show only images, no videos or anything else
-                intent.setType("image*//*");
-                intent.setAction(Intent.ACTION_GET_CONTENT);
-                // Always show the chooser (if there are multiple options available)
-                startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE);
+
+                intent = new Intent(Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+                startActivityForResult(intent, IMG_RESULT);
+
             }
         });
 
-        detectionProgressDialog = new ProgressDialog(this);
     }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == PICK_IMAGE && resultCode == RESULT_OK && data != null && data.getData() != null) {
-            Uri uri = data.getData();
-            try {
-                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
-                ImageView imageView = (ImageView) findViewById(R.id.imageView1);
-                imageView.setImageBitmap(bitmap);
-            } catch (IOException e) {
-                e.printStackTrace();
+        try {
+
+            if (requestCode == IMG_RESULT && resultCode == RESULT_OK
+                    && null != data) {
+
+
+                Uri URI = data.getData();
+                String[] FILE = { MediaStore.Images.Media.DATA };
+
+
+                Cursor cursor = getContentResolver().query(URI,
+                        FILE, null, null, null);
+
+                cursor.moveToFirst();
+
+                int columnIndex = cursor.getColumnIndex(FILE[0]);
+                ImageDecode = cursor.getString(columnIndex);
+                cursor.close();
+
+                imageViewLoad.setImageBitmap(BitmapFactory
+                        .decodeFile(ImageDecode));
+
+                Toast.makeText(this, "Image Selected", Toast.LENGTH_LONG)
+                        .show();
+
             }
+        } catch (Exception e) {
+            Toast.makeText(this, "Please try again", Toast.LENGTH_LONG)
+                    .show();
         }
+
     }
+
+    public String[] getFILE()
+    {
+        return FILE;
+    }
+
 }
